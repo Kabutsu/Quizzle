1a902bf73010255719999662289f986dd1fc4803
Sam <samjameslaw@gmail.com>
2020-04-30 22:21:50 +0100

    Added background audio

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioClip Track;
+    [SerializeField] private AudioSource Source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Source.clip = Track;
+        Source.loop = true;
+        Source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
